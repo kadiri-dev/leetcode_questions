263107705c316c4c48cbc033e2ffb11067608d32
kadiri-dev <78195306+kadiri-dev@users.noreply.github.com>
2021-01-28 22:47:57 -0800

    Create two_pointer-icici.cpp

---
+++ two_pointer-icici.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Bidding {
+    public:
+        vector <int> returnBestBid(vector<int> biddingArr, vector<int> percentArr, int sellerQuote){
+            vector<int> bestbidIndices;
+            int maxBid;
+
+            
+        }
+
+};
